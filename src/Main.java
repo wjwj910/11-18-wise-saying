c473d5676d0c66e3f3de68a8c959cb90b12098b9
wjwj910 <wjwj910@gmail.com>
2024-11-19 14:54:20 +0900

    chapter5 commit

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,25 @@ public class Main {
         app.run();
     }
 }
+class Quote{
+    int id;
+    String wise;
+    String author;
+
+    Quote(int id, String wise, String author) {
+        this.id = id;
+        this.wise = wise;
+        this.author = author;
+    }
+}
 
 class App{
+    private Quote[] quotes = new Quote[100];
+    private int count = 0;
+
     public void run(){
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        int id = 0;
 
         while (true){
             System.out.print("명령) ");
@@ -25,7 +38,17 @@ class App{
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                System.out.println((++id) + "번 명언이 등록되었습니다.");
+                quotes[count] = new Quote(count + 1, wise, author);
+
+                System.out.println((count + 1) + "번 명언이 등록되었습니다.");
+                count++;
+            }else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = count - 1; i >= 0; i--) {
+                    Quote quote = quotes[i];
+                    System.out.println(quote.id + " / " + quote.author + " / " + quote.wise);
+                }
             }
 
         }
